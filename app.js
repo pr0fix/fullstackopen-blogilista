a948e71291793359858caf65a88b2f52966218bc
pr0fix <bgx152@myy.haaga-helia.fi>
2024-03-23 13:44:29 +0200

    osa4 tehtava 4.2

---
+++ app.js
@@ -0,0 +1,30 @@
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const blogsRouter = require("./controllers/blogs");
+const middleware = require("./utils/middleware");
+const {info, error} = require("./utils/logger");
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+
+info("connecting to MongoDB database");
+
+mongoose.connect(config.MONGODB_URI).then(() => {
+    info("connected to MongoDB");
+}).catch((err) => {
+    error("error connecting to MongoDB", err.message);
+});
+
+app.use(cors());
+app.use(express.static("dist"));
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use("/api/blogs", blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app
--- index.js
+++ index.js
@@ -1,50 +1,7 @@
-require("dotenv").config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require("./app");
+const config = require("./utils/config");
+const {info} = require("./utils/logger");
 
-app.use(cors())
-app.use(express.json())
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-}, {versionKey: false})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-    .catch()
-})
-
-app.post('/api/blogs', (request, response) => {
-  const body = request.body
-
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes
-  })
-
-  blog.save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  info(`Server running on port ${config.PORT}`)
 })

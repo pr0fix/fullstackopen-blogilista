c845ea05450fa68306f849c81a994069e484b3d2
pr0fix <bgx152@myy.haaga-helia.fi>
2024-03-25 16:32:44 +0200

    osa4 tehtava 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
     return 1;
 }
 
+const totalLikes = (blogs) => {
+    let sum = 0;
+
+    for(let i = 0; i< blogs.length; i++) {
+        sum += blogs[i].likes
+    }
+    return sum;
+};
+
 module.exports = {
-    dummy
-}
+    dummy, totalLikes
+};

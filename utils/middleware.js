a9f7fc9d7618de9200f3cdf4a13f49838033fe66
pr0fix <bgx152@myy.haaga-helia.fi>
2024-05-14 18:41:52 +0300

    osa4 tehtava 4.15 ja 4.17

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,30 +1,37 @@
-const {info, error} = require("./logger")
+const { info, error } = require("./logger");
 
 const requestLogger = (request, response, next) => {
-    info("Method:", request.method);
-    info("Path:", request.path);
-    info("Body:", request.body);
-    info("---");
-    next();
+  info("Method:", request.method);
+  info("Path:", request.path);
+  info("Body:", request.body);
+  info("---");
+  next();
 };
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({error: "unknown endpoint"});
+  response.status(404).send({ error: "unknown endpoint" });
 };
 
 const errorHandler = (err, request, response, next) => {
-    error(err.message);
+  error(err.message);
 
-    if(err.name === "CastError") {
-        return response.status(400).send({err: "malformatted id"});
-    } else if (err.name === "ValidationError") {
-        return response.status(400).json({error: err.message});
-    };
-    next(err);
+  if (err.name === "CastError") {
+    return response.status(400).send({ err: "malformatted id" });
+  } else if (err.name === "ValidationError") {
+    return response.status(400).json({ error: err.message });
+  } else if (
+    err.name === "MongoServerError" &&
+    err.message.includes("E11000 duplicate key error")
+  ) {
+    return response
+      .status(400)
+      .json({ error: "expected `username` to be unique" });
+  }
+  next(err);
 };
 
 module.exports = {
-    requestLogger,
-    unknownEndpoint,
-    errorHandler
+  requestLogger,
+  unknownEndpoint,
+  errorHandler,
 };

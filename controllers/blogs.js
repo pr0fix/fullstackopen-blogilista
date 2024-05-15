dd1e65190ef03b0dc2aa1654556f96c1a0391320
pr0fix <bgx152@myy.haaga-helia.fi>
2024-05-15 09:28:06 +0300

    osa4 tehtava 4.19

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,15 @@
+const jwt = require("jsonwebtoken");
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
+
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "");
+  }
+  return null;
+};
 
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
@@ -35,10 +44,15 @@ blogsRouter.put("/:id", async (request, response) => {
 blogsRouter.post("/", async (request, response) => {
   const body = request.body;
 
-  const user = await User.findById(body.userId);
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({error: "token invalid"})
+  }
+
+  const user = await User.findById(decodedToken.id);
 
   if (!user) {
-    return response.status(400).json({error: "User not found"})
+    return response.status(400).json({ error: "User not found" });
   }
 
   const blog = new Blog({
--- utils/middleware.js
+++ utils/middleware.js
@@ -19,13 +19,12 @@ const errorHandler = (err, request, response, next) => {
     return response.status(400).send({ err: "malformatted id" });
   } else if (err.name === "ValidationError") {
     return response.status(400).json({ error: err.message });
-  } else if (
-    err.name === "MongoServerError" &&
-    err.message.includes("E11000 duplicate key error")
-  ) {
-    return response
-      .status(400)
-      .json({ error: "expected `username` to be unique" });
+  } else if (err.name === "MongoServerError" && err.message.includes("E11000 duplicate key error")) {
+    return response.status(400).json({ error: "expected `username` to be unique" });
+  } else if (err.name === "JsonWebTokenError") {
+    return response.status(400).json({ error: "token missing or invalid" });
+  } else if (err.name === "TokenExpiredError") {
+    return response.status(401).json({error: "token expired",});
   }
   next(err);
 };
